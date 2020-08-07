27fc9bc42326853dceb214105c50aeabb1823f51
quahjieren@gmail.com <quahjieren@gmail.com>
2020-08-07 23:02:55 +0800

    Updated load testing with vegeta

--- src/api/tests/vegeta/scripts/attack.sh
+++ src/api/tests/vegeta/scripts/attack.sh
@@ -1,3 +1,2 @@
 cd ../targets/
-
-cat targets_users.http | vegeta attack -rate 0 -max-workers 100 -duration 5s | tee results.bin | vegeta report
+cat targets_users.http | vegeta attack -rate 0 -max-workers 100 -duration 30s | tee results.bin | vegeta report
