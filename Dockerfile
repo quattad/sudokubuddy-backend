b958123be536efecedfb3c7d0b702ae05af9f1fa
quahjieren@gmail.com <quahjieren@gmail.com>
2020-09-26 17:08:52 +0800

    Update Dockerfile, removed segregation of dev and prod env vars

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 ## DOCKER BUILD CMDS
-## docker build -t sudokubuddy-backend .
-## docker run -p 8080:8080 --network="host" sudokubuddy-backend
+
+
+## docker build -t sudokubuddy-backend:latest .
+
+## Run  
+# docker run -p 8080:8080 --network="test-network" sudokubuddy-backend:latest
+
 ## For dev environment, connect to local db via 192.168.1.99:3306 where <host_ip>:<port>
 ## Make HTTP request to 192.168.99.100:8080 where <docker_machine_ip>:<forwarded_port>
 
@@ -10,26 +15,13 @@ FROM golang:latest
 ## Install application globally
 RUN go get -u github.com/quattad/sudokubuddy-backend/src/main
 
-# Set dev/production env
-ENV ENV_CONFIG=development
-
 ## Configure environment variables
-# DEVELOPMENT
-# ENV API_BASE_URL=http://localhost:8080
-# ENV PORT=8080
-# ENV DB_DRIVER=mysql
-# ENV DB_USER=root
-# ENV DB_PASSWORD=Zenosparadox666!
-# ENV DB_NAME=tcp(192.168.1.99:3306)/sudokubuddy
-# ENV API_SECRET=WhHYReR5BzKkmUeoc5gIZOTmXNjLivlZ
-# ENV ROOT_URL=github.com/quattad/sudokubuddy-backend
-
-# PRODUCTION
 ENV PORT=8080
 ENV DB_DRIVER=mysql
 ENV DB_USER=root
-ENV DB_PASSWORD=Zenosparadox666!
-ENV DB_NAME=tcp(sudokubuddy-db.cspmexpcmnes.us-east-2.rds.amazonaws.com:3306)/sudokubuddy
+ENV DB_PASSWORD=Sudokubuddy123!
+ENV DB_NAME=tcp(172.18.0.2:3306)/sudokubuddy
+# ENV DB_NAME=tcp(sudokubuddy-db.cspmexpcmnes.us-east-2.rds.amazonaws.com:3306)/sudokubuddy
 ENV API_SECRET=WhHYReR5BzKkmUeoc5gIZOTmXNjLivlZ
 ENV ROOT_URL=github.com/quattad/sudokubuddy-backend
 
--- src/api/config/config.go
+++ src/api/config/config.go
@@ -12,52 +12,57 @@ import (
 // DBDRIVER stores type of db used - in this case MySQL
 // SECRETKEY stores the hash key of the API used to generate the jwt
 var (
-	err       error
-	PORT      int
-	DBDRIVER  string
-	DBURL     string
-	SECRETKEY []byte
+	err         error
+	PORT        int
+	DBDRIVER    string
+	DBURL       string
+	SECRETKEY   []byte
+	DB_USER     string
+	DB_PASSWORD string
+	DB_NAME     string
 )
 
 // Load fetches environment variables and assigns them to respective variables
 func Load() {
 	// os.ExpandEnv url must be absolute path
-	env := os.Getenv("ENV_CONFIG")
+	// env := os.Getenv("ENV_CONFIG")
 
-	if env == "production" {
+	// if env == "production" {
 
-		fmt.Println("Production environment detected, loading environment variables from Dockerfile...")
+	PORT, err = strconv.Atoi(os.Getenv("PORT"))
 
-		PORT, err = strconv.Atoi(os.Getenv("PORT"))
-
-		if err != nil {
-			log.Println(err)
-			PORT = 8080
-		}
-
-		DBDRIVER = os.Getenv("DB_DRIVER")
-		DBURL = fmt.Sprintf("%s:%s@%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-		SECRETKEY = []byte(os.Getenv("API_SECRET"))
-
-	} else {
-
-		fmt.Println("Development environment detected, loading environment variables from Dockerfile...")
-
-		// err := godotenv.Load(os.ExpandEnv("$GOPATH/src/github.com/quattad/sudokubuddy-backend/env/.env"))
-
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-
-		PORT, err = strconv.Atoi(os.Getenv("PORT"))
-
-		if err != nil {
-			log.Println(err)
-			PORT = 8000
-		}
-
-		DBDRIVER = os.Getenv("DB_DRIVER")
-		DBURL = fmt.Sprintf("%s:%s@%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-		SECRETKEY = []byte(os.Getenv("API_SECRET"))
+	if err != nil {
+		log.Println(err)
+		PORT = 8080
 	}
+
+	DBDRIVER = os.Getenv("DB_DRIVER")
+	DB_USER = os.Getenv("DB_USER")
+	DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	DB_NAME = os.Getenv("DB_NAME")
+
+	DBURL = fmt.Sprintf("%s:%s@%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	SECRETKEY = []byte(os.Getenv("API_SECRET"))
+
+	// 	} else {
+
+	// 		fmt.Println("Development environment detected, loading environment variables from .env file...")
+
+	// 		err := godotenv.Load(os.ExpandEnv("$GOPATH/src/github.com/quattad/sudokubuddy-backend/env/.env"))
+
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+
+	// 		PORT, err = strconv.Atoi(os.Getenv("PORT"))
+
+	// 		if err != nil {
+	// 			log.Println(err)
+	// 			PORT = 8000
+	// 		}
+
+	// 		DBDRIVER = os.Getenv("DB_DRIVER")
+	// 		DBURL = fmt.Sprintf("%s:%s@%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	// 		SECRETKEY = []byte(os.Getenv("API_SECRET"))
+	// 	}
 }
